e5baa017b585cb76d69a06bc794adaa3a9607838
Anteneh Yehalem <artificialintel02@gmail.com>
2025-10-02 00:34:58 +0300

    updating index.py file

---
+++ add.py
@@ -0,0 +1,4 @@
+name = "anteneh"
+age = 27
+
+print(f"Name: {name}\nAge: {age}\n")
--- index.py
+++ index.py
@@ -1,1 +1,6 @@
 print("trying github desktop")
+
+name = "GitHub Desktop"
+version = "4.0.3"
+release = "1"
+print((f"Name: {name}\nVersion: {version}\nRelease: {release}\n"))
